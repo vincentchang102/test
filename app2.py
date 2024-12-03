52160057a256b9b7e9621d69af087dd3fddffb76
vincentchang102 <57772305+vincentchang102@users.noreply.github.com>
2024-12-02 20:59:45 -0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -109,6 +109,8 @@ with col2:
                 if "reply" in response_json:
                     result = response_json["reply"]
                     st.write(f"Response from backend: {result}")
+                else:s
+                    st.write("none")
             else:
                 st.write(f"Error with backend request {res.text}")
                 st.write(res.status_code)
